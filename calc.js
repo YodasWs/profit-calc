63e95b8664a75ad0e6224ca182433a72989eba0c
Samuel Grundman <samuel@yodas.ws>
2015-07-14 11:55:21 -0400

    Use simple example for first calculations
    Using a simple example with two ingredients, show profit from two recipes.

--- calc.js
+++ calc.js
@@ -19,4 +19,48 @@ calc.findRecipes = function (item) {
 		// TODO: Return Intersection
 	}
 	// TODO: Find Recipes that Require Given Item
+	return recipes
 }
+
+calc.profit = function() {
+	var items = [
+		{name:'Decent Cucumber',qty:5,revenue:160},
+		{name:'Good Egg',qty:5,revenue:80}
+	], output = Z('output')
+	//var recipes = calc.findRecipes(item)
+	var recipes = [
+		{name:'Stir Fry',revenue:244},
+		{name:'Good Mayonnaise',revenue:100},
+		{name:'Stir Fry + Good Mayonnaise'},
+		{name:'Vegetable Salad',revenue:419}
+	]
+	recipes[0].profit = recipes[0].revenue - items[0].revenue
+	recipes[1].profit = recipes[1].revenue - items[1].revenue
+	recipes[2].profit = recipes[0].profit + recipes[1].profit
+	recipes[3].profit = recipes[3].revenue - items[0].revenue - items[1].revenue
+	recipes.forEach(function(r,i) {
+		var div = Z('<div>')
+		div.append('<h4>' + recipes[i].name)
+		var txt = ''
+		switch (r.name) {
+			case 'Stir Fry':
+				txt = items[0].name
+				break;
+			case 'Good Mayonnaise':
+				txt = items[1].name
+				break;
+			case 'Stir Fry + Good Mayonnaise':
+				txt = items[0].name + ', ' + items[1].name
+				break;
+			case 'Vegetable Salad':
+				txt = items[0].name + ', ' + items[1].name
+				break;
+		}
+		txt += ': ' + recipes[i].profit
+		div.append('<div>' + txt)
+		if (recipes[i].profit < 0)
+			div.children('div').addClass('loss')
+		output.append(div)
+	})
+}
+calc.profit()
